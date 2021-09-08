ad091bb3e85001e408e826017a4572da0805fa27
Jan Ruman <janrumik@gmail.com>
2021-09-08 09:56:25 +0200

    add /single/interval request implementation

--- src/server/main.py
+++ src/server/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 import pandas as pd
 
+from src.aggregator import IntervalAggregator
+
 import src.server.single.precomputed as sp
+import src.server.single.interval as si
 
 app = FastAPI()
 
@@ -22,4 +25,26 @@ async def single_precomputed(request: sp.Request):
     speaker_df.apply(lambda row: sp.update_response(response, row) , axis=1)
 
     return sp.Response(**response)
+
+@app.post("/single/interval", response_model=si.Response)
+async def single_precomputed(request: si.Request):
+    input_dir = "samples/sample_audio_statistics_output/"
+    output_dir = "samples/sample_statistics_output/"
+    start = request.start
+    end = request.end
+
+    aggregator = IntervalAggregator(input_dir, output_dir, start, end)
+    data_df = aggregator.aggregate()
+    speaker_df = data_df.loc[data_df["speaker"] == request.MoP]
+
+    response = {
+        "speaking_time": dict(),
+        "relative_diff": dict(),
+        "unanchored": dict(),
+        "wpm": dict()
+    }
+
+    speaker_df.apply(lambda row: si.update_response(response, row) , axis=1)
+
+    return si.Response(**response)
 
